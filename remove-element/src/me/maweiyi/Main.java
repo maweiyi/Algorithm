f9937c8ecb7589cd3474dd480f91e740f90e1a01
maweiyi <ma823285807@gmail.com>
2016-09-07 21:05:55 +0800

    no!

--- remove-element/src/me/maweiyi/Main.java
+++ remove-element/src/me/maweiyi/Main.java
@@ -3,14 +3,11 @@ package me.maweiyi;
 public class Main {
 
     public static int removeElement(int[] A, int elem) {
-        if (A == null || A.length == 0)
-            return 0;
+
         int len = 0;
         for (int i = 0; i < A.length; i++) {
             if (A[i] != elem) {
-                if (A[len] != A[i])
-                    A[len] = A[i];
-                len++;
+                A[len++] = A[i];
             }
         }
 
