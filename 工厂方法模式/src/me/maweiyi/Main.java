d28e4225cce843082a48f28dd9862d1b1d58d24d
maweiyi <ma823285807@gmail.com>
2016-09-27 14:46:12 +0800

    普通工厂模式

--- 工厂方法模式/src/me/maweiyi/Main.java
+++ 工厂方法模式/src/me/maweiyi/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SendFactory factory = new SendFactory();
+        Sender sender = factory.produce("sms");
+        sender.sender();
     }
 }
