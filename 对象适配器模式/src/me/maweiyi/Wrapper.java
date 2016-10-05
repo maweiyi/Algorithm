8fe4c28e6a3c6e932d6617eab09506a71c37116b
maweiyi <ma823285807@gmail.com>
2016-10-05 19:03:28 +0800

    对象适配器模式

---
+++ 对象适配器模式/src/me/maweiyi/Wrapper.java
@@ -0,0 +1,23 @@
+package me.maweiyi;
+
+/**
+ * Created by maweiyi on 10/5/16.
+ */
+public class Wrapper implements Targetable {
+
+    private Source source;
+
+    public Wrapper(Source source){
+        super();
+        this.source = source;
+    }
+    @Override
+    public void method2() {
+        System.out.println("this is the targetable method!");
+    }
+
+    @Override
+    public void method1() {
+        source.method1();
+    }
+}
