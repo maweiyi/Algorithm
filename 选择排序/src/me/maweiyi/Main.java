b4dcb479dab6bfa4753f989289995b4d0ba63ffb
maweiyi <ma823285807@gmail.com>
2016-11-14 15:39:05 +0800

    修改选择排序算法

--- 选择排序/src/me/maweiyi/Main.java
+++ 选择排序/src/me/maweiyi/Main.java
@@ -25,16 +25,23 @@ public class Main {
     }
 
     public static void xueze(){
-        int[] a = {6, 3, 5, 7, 0, 4, 2, 1};
-        int tmp;
-        for (int i = 0; i < a.length; i++) {
+        int[] a = {3, 100, 7, 8, 4, 1, 2};
+
+        for (int i = 0; i < a.length - 1; i++) {
+            int pp = a[i], k = -1, tmp;//选出一个标志
             for (int j = i; j < a.length; j++) {
-                if (a[i] > a[j]) {
-                    tmp = a[i];
-                    a[i] = a[j];
-                    a[j] = tmp;
+                if (pp > a[j]) {
+                    pp = a[j];//记录最小值
+                    k = j;//记录最小值的下标
                 }
             }
+
+            if (k != -1) {
+                tmp = a[i];
+                a[i] = a[k];
+                a[k] = tmp;
+            }
+            
         }
 
         for (int i = 0; i < a.length; i++) {
